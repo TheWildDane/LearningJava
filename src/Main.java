95986e063ace128d2a3877835fa5ec51fd3e04ce
Steffen Rasmussen <42993915+TheWildDane@users.noreply.github.com>
2018-09-05 14:31:21 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import static java.lang.System.out;
+
+public class Main {
+
+    /*
+    invariable part
+     */
+    public static void main(String[] args) {
+        System.out.print("Ács András ");
+        System.out.print("Is The ");
+        System.out.print("Best Teacher \n");
+        {
+            // difference between Print and Prinln
+        }
+
+        System.out.println("Please");
+        System.out.println("Give Me");
+        System.out.println("an A+ Please");
+
+        /*
+        A String variable named s is created.
+        This variable can store text.
+
+        An int variable named x is created.
+        This variable can store integers.
+
+        int variables named a, b, c and d are created.
+        These variables can store integers.
+        */
+        String n = "My name is Steffen,";
+        int age = 29;
+        String c = " i live in Fuglebjerg";
+
+        System.out.println(n + c + " and i am " + age + " years old");
+    }
+    }
